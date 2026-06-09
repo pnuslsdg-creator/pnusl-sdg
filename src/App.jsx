120b04075d0093cd123e59501a55a2308de445fc
PNUSL SDG <pnuslsdg@gmail.com>
2026-06-09 14:04:02 +0800

    UPDATED PNUSL-SDG

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function Header() {
     <header className="top-header">
       <div className="header-brand">
         <div className="header-text">
-          <p>Philippine Normal University South Luzon</p>
+          <p>The Technology and Livelihood Education Hub</p>
         </div>
       </div>
     </header>
